ef837691238271bf762465b58e35be8e96f913fa
felix <felixchoy97@gmail.com>
2024-07-01 23:19:52 -0400

    Modified robots.txt and added React-Helmet dependency to add a meta tag to prevent indexing of website.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Home from './Pages/Home/Home'
 import Portfolio from './Pages/Portfolio/Portfolio'
 import About from './Pages/About/About'
 import Contact from './Pages/Contact/Contact'
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
     <>
+        <Helmet>
+          <meta name="robots" content="noindex" />
+        </Helmet>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
